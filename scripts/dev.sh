43b571733959a69c71000bafb753aed59f557801
Phil Labrum <philip.labrum@gmail.com>
2025-08-03 20:58:10 -0400

    add local postgres docker

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
+echo "Starting dockerized Postgres..."
+docker compose -f docker-compose-development.yml up -d
+
 echo "Starting backend and frontend..."
 
-# Kill both processes on exit or Ctrl+C
-trap 'kill 0' SIGINT SIGTERM EXIT
+# Kill everything on Ctrl+C or script exit
+trap 'echo "Stopping..."; kill 0; docker compose -f docker-compose-development.yml down' SIGINT SIGTERM EXIT
 
 # Start backend (Litestar)
 (
